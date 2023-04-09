3049cfbf722550561301e286d953af8d3825fcbc
Eduardo668 <eduardosehn20@gmail.com>
2023-04-09 18:00:43 -0300

    New delete method with custom query

--- backend/src/main/java/com/pdf/library/repository/UserRepository.java
+++ backend/src/main/java/com/pdf/library/repository/UserRepository.java
@@ -2,10 +2,17 @@ package com.pdf.library.repository;
 
 import com.pdf.library.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
+
+    @Modifying
+    @Query("DELETE FROM UserModel u WHERE u = ?1")
+    UserModel deleteUserModel(UserModel deletedUser);
+
 }
